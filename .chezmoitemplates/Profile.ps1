a92c24326e218764a661c864754205647fa45ce6
Chris Reinker <creinker@gmail.com>
2022-07-10 14:27:16 -0700

    Initial commit, migrated from dotbot

---
+++ .chezmoitemplates/Profile.ps1
@@ -0,0 +1,12 @@
+Set-PSReadLineOption -EditMode Vi
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+New-Alias -Name k -Value kubectl
