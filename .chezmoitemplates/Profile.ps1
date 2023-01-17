49340c770eccdb623df9c29cbc4c02e7c1794556
Chris Reinker <creinker@gmail.com>
2023-01-17 10:30:07 -0800

    PS Profile: configure choco tab-completions, scoop-search

--- .chezmoitemplates/Profile.ps1
+++ .chezmoitemplates/Profile.ps1
@@ -1,3 +1,5 @@
+# Use VI edit mode with PSReadLine
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
 Set-PSReadLineOption -EditMode Vi
 function OnViModeChange {
     if ($args[0] -eq 'Command') {
@@ -8,5 +10,19 @@ function OnViModeChange {
         Write-Host -NoNewLine "`e[5 q"
     }
 }
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Use scoop-search to improve search speed: https://github.com/shilangyu/scoop-search
+Invoke-Expression (&scoop-search --hook)
+
+# Alias for kubernetes
 New-Alias -Name k -Value kubectl
